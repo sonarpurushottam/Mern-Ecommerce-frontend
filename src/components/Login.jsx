0af3e96436b93f92ee899db61df5f45c5bf1c2e3
Purushottam Sonar <124604807+sonarpurushottam@users.noreply.github.com>
2024-08-21 03:25:07 +0530

    login done

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -51,8 +51,8 @@ const Login = () => {
       );
     },
   });
-  
 
+  window.location.reload();
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4">
       <Toaster />
