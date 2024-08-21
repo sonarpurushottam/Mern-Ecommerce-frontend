cb7a2b640696ee7fd578aae32ba6859bc97c5e36
Purushottam Sonar <124604807+sonarpurushottam@users.noreply.github.com>
2024-08-21 20:22:23 +0530

    brand fetch done

--- src/hooks/useBrands.jsx
+++ src/hooks/useBrands.jsx
@@ -8,18 +8,32 @@
 //     enabled: !!categoryId,
 //   });
 // };
+// import { useQuery } from "@tanstack/react-query";
+// import axios from "axios";
+
+// export const useBrands = (categoryId) => {
+//   return useQuery({
+//     queryKey: ["brands", categoryId],
+//     queryFn: async () => {
+//       if (!categoryId) throw new Error("No category ID provided");
+//       const { data } = await axios.get(`http://localhost:5000/api/brands/getByCategory/${categoryId}`);
+//       return data;
+//     },
+//     enabled: !!categoryId,
+//   });
+// };
+
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import axiosInstance from "../api/axiosInstance";
 
 export const useBrands = (categoryId) => {
   return useQuery({
     queryKey: ["brands", categoryId],
     queryFn: async () => {
       if (!categoryId) throw new Error("No category ID provided");
-      const { data } = await axios.get(`http://localhost:5000/api/brands/getByCategory/${categoryId}`);
+      const { data } = await axiosInstance.get(`/brands/getByCategory/${categoryId}`);
       return data;
     },
     enabled: !!categoryId,
   });
 };
-
